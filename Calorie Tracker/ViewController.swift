5e0460ed23fec548d41206459ee9758e9a6bd917
Isabel Heyninck <iheyn001@fiu.edu>
2022-07-20 16:21:37 -0400

    Initial Commit

---
+++ Calorie Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by user220668 on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
