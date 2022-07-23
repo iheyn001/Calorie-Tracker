74dde3bee81f815628435efb806b8cfbdc327ed0
Isabel Heyninck <iheyn001@fiu.edu>
2022-07-23 16:44:16 -0400

    Homepage view set up and basic info display.

--- Calorie Tracker/ViewController.swift
+++ Calorie Tracker/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var dailyCalories = 0
+    var weeklyCalories = 0
+    var avgWeeklyCalories = 0
+    var daysUsed = 1
+    
+    @IBOutlet weak var daily: UILabel!
+    @IBOutlet weak var weekly: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if weeklyCalories != 0 {
+            avgWeeklyCalories = weeklyCalories/daysUsed
+        }
+        
+        daily.text = String(dailyCalories)
+        weekly.text = String(avgWeeklyCalories)
     }
-
-
+    
 }
 
